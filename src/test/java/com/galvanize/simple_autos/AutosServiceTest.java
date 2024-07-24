6e2aee2a05b8840ba59d8668e073106bb9036da4
inga.isom.kwlv <inga.isom.kwlv@statefarm.com>
2024-07-24 13:03:12 -0400

    cleaned up

--- src/test/java/com/galvanize/simple_autos/AutosServiceTest.java
+++ src/test/java/com/galvanize/simple_autos/AutosServiceTest.java
@@ -1,6 +1,5 @@
 package com.galvanize.simple_autos;
 
-import org.assertj.core.api.Assert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -8,17 +7,12 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Arrays;
-import java.util.Optional;
 
-import static org.apache.logging.log4j.ThreadContext.isEmpty;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.util.AssertionErrors.assertEquals;
-import static org.springframework.test.util.AssertionErrors.assertTrue;
-import static org.springframework.test.web.servlet.result.StatusResultMatchersExtensionsKt.isEqualTo;
 
 @ExtendWith(MockitoExtension.class)
 class AutosServiceTest {
