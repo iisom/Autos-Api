bc32edb7f05cd568bb0bfd45df45676e67211bc3
inga.isom.kwlv <inga.isom.kwlv@statefarm.com>
2024-07-24 17:20:11 -0400

    db setup

--- src/test/java/com/galvanize/simple_autos/SimpleAutosApplicationTests.java
+++ src/test/java/com/galvanize/simple_autos/SimpleAutosApplicationTests.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.*;
+import org.springframework.test.context.TestPropertySource;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,10 +16,11 @@ import java.util.Random;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource(locations = "classpath:application-test.properties")
 class SimpleAutosApplicationTests {
 
     @Autowired
-    private TestRestTemplate restTemplate;
+    TestRestTemplate restTemplate;
 
 
     @Autowired
