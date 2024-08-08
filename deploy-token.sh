ced56e919de593c5e0d77f0d52695077f3b26db6
inga.isom.kwlv <inga.isom.kwlv@statefarm.com>
2024-08-08 16:44:06 -0400

    updated config/deploy/service/secret --- added deploy

---
+++ deploy-token.sh
@@ -0,0 +1,4 @@
+kubectl create secret docker-registry ii-simple-autos-token \
+    --docker-username="iisom" \
+    --docker-password="gldt-gB7b5ssFFWL_-tJFss9Q" \
+    --docker-server="registry.gitlab.galvanize.com"
